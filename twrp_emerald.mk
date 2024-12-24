fcd57b2ddc3f7f87e21e69d3c599ae268dfa4c5f
V3NCO <75168136+V3NCO@users.noreply.github.com>
2024-12-24 14:02:00 +0100

    Omni to twrp

--- twrp_emerald.mk
+++ twrp_emerald.mk
@@ -9,14 +9,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common Twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from emerald device
 $(call inherit-product, device/xiaomi/emerald/device.mk)
 
 PRODUCT_DEVICE := emerald
-PRODUCT_NAME := omni_emerald
+PRODUCT_NAME := twrp_emerald
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := emerald
 PRODUCT_MANUFACTURER := xiaomi
